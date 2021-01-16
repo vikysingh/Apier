84bf4f01135db1a81f7f9b3a4049a40c01e0f7f6
Vikram <Vikram Singh>
2021-01-16 15:01:39 +0100

    aggiornato _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,15 +3,26 @@ import "../styles/globals.css";
 
 import Head from "next/head";
 
-const titolo = "Apier"
-const descrizione = "Un app store per giochi ed app web progressive!"
-const url = "https://apier.netlify.app"
-const colore = "#6C63FF"
+const titolo = "Apier";
+const descrizione = "Un app store per giochi ed app web progressive!";
+// const url = "https://apier.netlify.app";
+const colore = "#6C63FF";
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="theme-color" content={colore} />
+        <meta name="description" content={descrizione} />
+        <meta name="og:title" property="og:title" content={titolo} />
+        <meta
+          name="og:description"
+          property="og:description"
+          content={descrizione}
+        />
+
         <link
           rel="apple-touch-icon"
           sizes="57x57"
@@ -81,43 +92,18 @@ function MyApp({ Component, pageProps }) {
           sizes="16x16"
           href="/static/icons/favicon-16x16.png"
         />
-        <link rel="manifest" href="/manifest.json" />
-
-        <meta name="application-name" content="Apier" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="apple-mobile-web-app-title" content={titolo} />
-        <meta name="description" content={descrizione} />
-        <meta name="format-detection" content="telephone=no" />
-        <meta name="mobile-web-app-capable" content="yes" />
         <meta
           name="msapplication-config"
           content="/static/icons/browserconfig.xml"
         />
+        <link rel="manifest" href="/static/manifest.json" />
         <meta name="msapplication-TileColor" content={colore} />
-        <meta name="msapplication-tap-highlight" content="no" />
-        <meta name="theme-color" content={colore} />
+        <meta
+          name="msapplication-TileImage"
+          content="/static/ms-icon-144x144.png"
+        />
 
-        <meta name="twitter:card" content="summary" />
-        <meta name="twitter:url" content={url} />
-        <meta name="twitter:title" content={titolo} />
-        <meta name="twitter:description" content={descrizione} />
-        <meta
-          name="twitter:image"
-          content={`${url}/static/icons/android-chrome-192x192.png`}
-        />
-        <meta property="og:type" content="website" />
-        <meta property="og:title" content={titolo} />
-        <meta property="og:description" content={descrizione} />
-        <meta property="og:site_name" content={titolo} />
-        <meta property="og:url" content={url} />
-        <meta
-          property="og:image"
-          content={`${url}/static/icons/apple-touch-icon.png`}
-        />
-        <meta name="msapplication-TileColor" content={colore} />
-        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
-        <meta name="theme-color" content={colore} />
+        <title>Apier</title>
       </Head>
       <Component {...pageProps} />
     </>
