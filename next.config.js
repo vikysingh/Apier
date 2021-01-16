7623bbe9c8ba86d5d9fcec3613803a528a9133b4
Vikram <Vikram Singh>
2021-01-16 14:37:30 +0100

    aggiunto icone e manifest e next-pwa

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 const withPlugins = require('next-compose-plugins')
-const withImages = require("next-images") 
+const withImages = require("next-images")
+const withPWA = require("next-pwa")
 
 module.exports = withPlugins(
-    [withImages]
+    [withImages],
+    [withPWA, {
+        pwa: {
+            dest: "public"
+        }
+    }]
 );
--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,127 @@
-import 'semantic-ui-css/semantic.min.css'
-import "../styles/globals.css"
+import "semantic-ui-css/semantic.min.css";
+import "../styles/globals.css";
+
+import Head from "next/head";
+
+const titolo = "Apier"
+const descrizione = "Un app store per giochi ed app web progressive!"
+const url = "https://apier.netlify.app"
+const colore = "#6C63FF"
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <link
+          rel="apple-touch-icon"
+          sizes="57x57"
+          href="/static/icons/apple-icon-57x57.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="60x60"
+          href="/static/icons/apple-icon-60x60.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="72x72"
+          href="/static/icons/apple-icon-72x72.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="76x76"
+          href="/static/icons/apple-icon-76x76.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="114x114"
+          href="/static/icons/apple-icon-114x114.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="120x120"
+          href="/static/icons/apple-icon-120x120.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="144x144"
+          href="/static/icons/apple-icon-144x144.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="152x152"
+          href="/static/icons/apple-icon-152x152.png"
+        />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/static/icons/apple-icon-180x180.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="192x192"
+          href="/static/icons/android-icon-192x192.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/static/icons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="96x96"
+          href="/static/icons/favicon-96x96.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/static/icons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/manifest.json" />
+
+        <meta name="application-name" content="Apier" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+        <meta name="apple-mobile-web-app-title" content={titolo} />
+        <meta name="description" content={descrizione} />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta
+          name="msapplication-config"
+          content="/static/icons/browserconfig.xml"
+        />
+        <meta name="msapplication-TileColor" content={colore} />
+        <meta name="msapplication-tap-highlight" content="no" />
+        <meta name="theme-color" content={colore} />
+
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:url" content={url} />
+        <meta name="twitter:title" content={titolo} />
+        <meta name="twitter:description" content={descrizione} />
+        <meta
+          name="twitter:image"
+          content={`${url}/static/icons/android-chrome-192x192.png`}
+        />
+        <meta property="og:type" content="website" />
+        <meta property="og:title" content={titolo} />
+        <meta property="og:description" content={descrizione} />
+        <meta property="og:site_name" content={titolo} />
+        <meta property="og:url" content={url} />
+        <meta
+          property="og:image"
+          content={`${url}/static/icons/apple-touch-icon.png`}
+        />
+        <meta name="msapplication-TileColor" content={colore} />
+        <meta name="msapplication-TileImage" content="/ms-icon-144x144.png" />
+        <meta name="theme-color" content={colore} />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
 }
 
-export default MyApp
+export default MyApp;
